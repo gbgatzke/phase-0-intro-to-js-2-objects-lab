8f184dd7c9d297cecb2d8fdb8811207a89e02a80
Galen Beck Gatzke <gbgatzke@gmail.com>
2022-12-14 09:55:53 -0700

    commit tree 5e77ecd963d29a793a6eda6b8baccbeeb23f92a6

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution in this file!
+
+/*const employee = {
+    name: "Sam",
+    streetAddress: "123 Real Ave",
+}
+
+function updateEmployeeWithKeyAndValue() {
+   return {
+    ...employee,
+    streetAddress: "11 Broadway"
+   }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+
+    return employee;
+}*/
+
+const employee = {
+    name:"",
+    streetAddress:"",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        streetAddress: "11 Broadway"
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+       employee.name = "Sam",
+       employee.streetAddress = "12 Broadway";
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+        delete newEmployee.name;
+        return {
+            newEmployee}
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee
+}
+
